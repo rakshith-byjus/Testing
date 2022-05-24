46898737a4467026263aa6a7565bbf788b48dee0
rakshith-byjus <rakshith.shetty4@byjus.com>
2022-05-24 10:33:54 +0530

    initial commit

---
+++ Assets/Scenes/Testing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using System;
+
+public class Testing : MonoBehaviour
+{
+    [CMSType]
+    class Data
+    {
+        //[CMSField]
+        private int data;
+    }
+
+    //public class CMSTypeAttribute : Attribute { }
+    //public class CMSFieldAttribute : Attribute { }
+
+}
